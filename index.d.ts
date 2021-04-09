156dca8f054f3d809b565b226656191ff134edf1
zoobestik <kb.chernenko@gmail.com>
2021-04-09 13:52:52 +0300

    fix; typings for different types

---
+++ index.d.ts
@@ -0,0 +1,37 @@
+import { Component, ReactNode } from 'react';
+
+export interface ReactKotlinPlaygroundProps {
+    className?: string;
+    children: ReactNode,
+    playground: (node: any, fn: any) => void,
+
+    onChange: (Event) => void
+    onConsoleOpen: (Event) => void
+    onConsoleClose: (Event) => void
+    getInstance: (Event) => void
+    getJsCode: (Event) => void
+    onRun: (Event) => void
+    onError: (Event) => void
+
+    version: string,
+    args: string | string[],
+    targetPlatform: "junit" | "canvas" | "js" | "java",
+    highlightOnly: null | "nocursor",
+    jsLibs: string,
+    autoIndent: boolean,
+    theme: string,
+    mode: "kotlin" | "js" | "java" | "groovy" | "xml" | "c" | "shell" | "swift" | "obj-c",
+    minCompilerVersion: string,
+    autocomplete: boolean,
+    highlightOnFly: boolean,
+    indent: number,
+    lines: boolean,
+    from: number,
+    to: number,
+    outputHeight: number,
+    matchBrackets: boolean,
+}
+
+declare const ReactKotlinPlayground: Component<ReactKotlinPlaygroundProps>;
+
+export default ReactKotlinPlayground;
